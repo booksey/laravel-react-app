901993968b1cd5a9d19145b6ef38151a03725ea3
kozsana <kozsanattila@gmail.com>
2020-12-09 19:53:37 +0100

    issue#1 - login endpoint created

---
+++ resources/views/loginForm.blade.php
@@ -0,0 +1,14 @@
+<h1>Login</h1>
+<h2>Fill the form!</h2>
+<h3>(Warning: it's dummy form...)</h3>
+<p>
+    <label>Username:</label>
+    <input type="text" placeholder="type..." />
+</p>
+<p>
+    <label>Password:</label>
+    <input type="text" placeholder="type..." />
+</p>
+<p>
+    <button>Submit</button>
+</p>
